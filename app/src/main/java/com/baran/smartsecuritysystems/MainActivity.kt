b4cdadece6f096e96d469b778cfb0bdf5d430606
baransolmaz <baransolmaz@hotmail.com>
2022-11-18 01:06:30 +0300

    Add Log in & Register

--- app/src/main/java/com/baran/smartsecuritysystems/MainActivity.kt
+++ app/src/main/java/com/baran/smartsecuritysystems/MainActivity.kt
@@ -1,21 +1,44 @@
 package com.baran.smartsecuritysystems
+
+import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.baran.smartsecuritysystems.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+        private const val STORAGE_PERMISSION_CODE = 101
+    }
     private lateinit var binding:ActivityMainBinding
-    //private lateinit var database: DatabaseReference
+    private var database: DatabaseReference =Firebase.database.reference
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,STORAGE_PERMISSION_CODE)
+        checkPermission(Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE)
 
         binding.mainSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
@@ -23,15 +46,91 @@ class MainActivity : AppCompatActivity() {
             //finish()
         }
         binding.mainLogIn.setOnClickListener {
-            val intent = Intent(this, SeparationActivity::class.java)
-            startActivity(intent)
-            //finish()
-        }
-        binding.mainHowTo.setOnClickListener {
-            /*val intent = Intent(this, SeparationActivity::class.java)
-            startActivity(intent)
-            //finish()*/
+
+            var inputUsername=binding.mainUsername.text.toString()
+            var inputPass=binding.mainPassword.text.toString()
+
+            if (inputUsername.isEmpty()||inputPass.isEmpty()){
+                Toast.makeText(this,"Please Enter Username/Password",Toast.LENGTH_SHORT).show()
+            }else{
+                database.child("Users").addListenerForSingleValueEvent( object : ValueEventListener {
+
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.hasChild(inputUsername)){
+                            val pass=dataSnapshot.child(inputUsername).child("passWord").value.toString()
+                            if (pass == inputPass) {
+                                Toast.makeText(this@MainActivity,"Log in successful!",Toast.LENGTH_SHORT).show()
+                                val intent =Intent(this@MainActivity, SeparationActivity::class.java)
+                                startActivity(intent)
+                            }
+                        }else{
+                            binding.mainUsername.text.clear()
+                            binding.mainPassword.text.clear()
+                            Toast.makeText(this@MainActivity,"Wrong username or password",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        Log.d("error", databaseError.message)
+
+                    }
+
+                })
+            }
         }
 
+    }
+    // Function to check and request permission.
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Camera Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Storage Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Storage Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    fun dialogHowTo(view: View) {
+
+        val alertDialog = AlertDialog.Builder(this).create()
+        alertDialog.setTitle("How To Use")
+        alertDialog.setMessage("1. Log in or Sign up\n" +
+                "2. Choose device type\n" +
+                "\t\t3.1 As Camera\n" +
+                "-To start streaming,press start button\n" +
+                "-To set timer,press timer button\n" +
+                "-To stop streaming,press stop button\n" +
+                "-To generate Qr code,press Qr button\n" +
+                "\t\t3.2 As Monitoring\n" +
+                "-You have to pair at least 1 camera\n" +
+                "-To monitor,press watch live\n" +
+                "-To see archive,press archive\n" +
+                "To unpair camera, long press pair button\n" +
+                "-You can see your account in profile menu\n" +
+                "-You can edit settings")
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { dialog, _ -> dialog.dismiss() }
+
+        alertDialog.show()
+
+        val btnPositive = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)
+
+        val layoutParams = btnPositive.layoutParams as LinearLayout.LayoutParams
+        layoutParams.weight = 10f
+        btnPositive.layoutParams = layoutParams
     }
 }
--- app/src/main/java/com/baran/smartsecuritysystems/SignUpActivity.kt
+++ app/src/main/java/com/baran/smartsecuritysystems/SignUpActivity.kt
@@ -5,12 +5,17 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.baran.smartsecuritysystems.databinding.ActivitySignUpBinding
+import com.google.firebase.FirebaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding:ActivitySignUpBinding
-    private lateinit var database: DatabaseReference
+    private var database: DatabaseReference=Firebase.database.reference
 
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,29 +26,46 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.createUserButton.setOnClickListener{
-            database=FirebaseDatabase.getInstance("https://smart-security-systems-72f11-default-rtdb.europe-west1.firebasedatabase.app/").reference
-            val user=getValues()
+            val name = binding.signName.text.toString()
+            val surname = binding.signSurname.text.toString()
+            val username = binding.signUsername.text.toString()
+            val pass = binding.signPassword.text.toString()
+            val mail = binding.signMail.text.toString()
+            val user=User(name, surname,username,pass,mail)
+            if(name.isEmpty()||surname.isEmpty()||username.isEmpty()||pass.isEmpty()||mail.isEmpty()){
+                Toast.makeText(this,"Please fill all fields!",Toast.LENGTH_SHORT).show()
+            }else{
+                database.child("Users").addListenerForSingleValueEvent( object : ValueEventListener {
 
-            database.child("Users").child(user.mail.toString()).setValue(user).addOnSuccessListener {
-                binding.signName.text.clear()
-                binding.signSurname.text.clear()
-                binding.signUsername.text.clear()
-                binding.signPassword.text.clear()
-                binding.signMail.text.clear()
-                Toast.makeText(this,"Successfully Saved!",Toast.LENGTH_LONG).show()
-                finish()
-            }.addOnFailureListener{
-                Toast.makeText(this,"Failed!",Toast.LENGTH_LONG).show()
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.hasChild(user.userName.toString())){
+                            Toast.makeText(this@SignUpActivity,"Username is already registered!",Toast.LENGTH_SHORT).show()
+                        }else{
+                            database.child("Users").child(user.userName.toString()).setValue(user).addOnSuccessListener {
+                                binding.signName.text.clear()
+                                binding.signSurname.text.clear()
+                                binding.signUsername.text.clear()
+                                binding.signPassword.text.clear()
+                                binding.signMail.text.clear()
+                                Toast.makeText(this@SignUpActivity,"Successfully Saved!",Toast.LENGTH_SHORT).show()
+                                finish()
+                            }.addOnFailureListener{
+                                Toast.makeText(this@SignUpActivity,"Failed!",Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        //TODO
+                        //Log.d("error", databaseError.message)
+
+                    }
+
+                })
+
             }
+
         }
     }
-    private fun getValues(): User {
-        val name = binding.signName.text.toString()
-        val surname = binding.signSurname.text.toString()
-        val username = binding.signUsername.text.toString()
-        val pass = binding.signPassword.text.toString()
-        val mail = binding.signMail.text.toString()
-        //TODO Mail database de var mı diye kontrol et yoksa ekle , varsa tekrar iste*/
-        return User(name,surname,username,pass,mail)
-    }
+
 }
