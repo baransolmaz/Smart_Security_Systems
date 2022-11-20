d148b43bc27ea316b96611bf0fe94ecc350ec786
baransolmaz <baransolmaz@hotmail.com>
2022-11-20 13:28:08 +0300

    Add Activity Qr

---
+++ app/src/main/java/com/baran/smartsecuritysystems/QrActivity.kt
@@ -0,0 +1,11 @@
+package com.baran.smartsecuritysystems
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QrActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr)
+    }
+}
