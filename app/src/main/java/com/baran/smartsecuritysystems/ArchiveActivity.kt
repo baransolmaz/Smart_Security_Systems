cc22e4a4fb4856f59518703600eeb63ee3e558b0
baransolmaz <baransolmaz@hotmail.com>
2022-12-30 18:51:02 +0300

    Firabase Storage Import

--- app/src/main/java/com/baran/smartsecuritysystems/ArchiveActivity.kt
+++ app/src/main/java/com/baran/smartsecuritysystems/ArchiveActivity.kt
@@ -1,16 +1,29 @@
 package com.baran.smartsecuritysystems
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.baran.smartsecuritysystems.databinding.ActivityArchiveBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+
 
 class ArchiveActivity : AppCompatActivity() {
-
+    private var camNum: Int =HomeActivity.PRESSED //Camera Number
+    private var deviceID: String? = HomeActivity.CHANNELS[camNum]
+    private lateinit var binding: ActivityArchiveBinding
+    private var storage : StorageReference = Firebase.storage.reference.child(deviceID.toString())
+    private lateinit var imageList:ArrayList<String>
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
-        setContentView(R.layout.activity_archive)
+        binding= ActivityArchiveBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val rec=binding.rec
+
+        val listRef = storage.child(deviceID.toString())
     }
 }
